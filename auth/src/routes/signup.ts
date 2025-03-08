f37d754792fa4881465db5c6df337f111ee572d8
juanLude <jmludevid@hotmail.com>
2025-03-09 09:43:28 +1100

    add middlewares

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
 
 router.post(
@@ -11,8 +11,17 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  (req: Request, res: Response): void => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+      // return;
+    }
     const { email, password } = req.body;
+    console.log("Creating a user...");
+    res.send({});
+    // return;
   }
 );
 
