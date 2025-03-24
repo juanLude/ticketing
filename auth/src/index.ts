e43ef78b7c3a1bc5a292d4e6595f1b51f1506b84
juanLude <jmludevid@hotmail.com>
2025-03-24 16:55:23 +1100

    add testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import cookieSession from "cookie-session";
-
 import mongoose from "mongoose";
-
-const app = express();
-app.set("trust proxy", true); // Trust traffic as secure even though it is coming from a proxy. Traffic is coming from Ingress Nginx, which is a proxy.
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // Disable encryption becuase JWT is already encrypted
-    secure: true, // Only use cookies over HTTPS
-  })
-);
-
-require("express-async-errors");
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-// app.all("*", async (req, res) => {
-//   throw new NotFoundError();
-// });
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
