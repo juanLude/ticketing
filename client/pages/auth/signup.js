4e51af8d767361fb621453d41a5d94cd039342fe
juanLude <jmludevid@hotmail.com>
2025-04-01 17:50:59 +1100

    set up urls where to make requests from (server/client)

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,30 +1,24 @@
-import { useState } from "react";
-import axios from "axios";
+import { use, useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
   const onSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await fetch("/api/users/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
-      }
-
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("Signup failed:", error.message);
-    }
+    await doRequest();
   };
+
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
@@ -46,6 +40,7 @@ export default () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
+      {errors}
       <button type="submit" className="btn btn-primary">
         Sign up
       </button>
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,28 @@
-export default () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Home</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // we are on the server hence requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      // this is the host name of the ingress controller
+      // httpSERVICENAME.NAMESPACE.svc.cluster.local
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      } // this is the host name of the ingress controller
+    );
+  } else {
+    // we are on the browser
+    // requests can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  // left at video 22 min 3:00
+  return {};
+};
+export default LandingPage;
