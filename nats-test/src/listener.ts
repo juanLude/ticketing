cfe26f06aacd6eae1b69a6403c31d7d8d9f64ccb
juanLude <jmludevid@hotmail.com>
2025-05-01 22:17:59 +1000

    update listener.ts

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,13 +1,28 @@
-import nats from "node-nats-streaming";
-
-const stan = nats.connect("ticketing", "123", { url: "http://localhost:4222" });
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
+//
+// This is a simple NATS streaming client that listens for messages on the "ticket:created" channel.
+// It connects to a NATS server, subscribes to the channel, and logs the received messages to the console.
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222",
+});
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
-  subscription.on("message", (msg) => {
-    console.log(`Message received: ${msg.getSequence()} - ${msg.getData()}`);
+  const options = stan.subscriptionOptions().setManualAckMode(true); // Set manual acknowledgment mode
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+    // Check if data is a Buffer and convert it to string if necessary
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()} with data: ${data}`);
+    }
+    msg.ack(); // Manually acknowledge the message
   });
   console.log("Listening for messages on ticket:created");
 });
