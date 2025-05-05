02e9fed114004c9158f963467305e24f5ede87dc
juanLude <jmludevid@hotmail.com>
2025-05-06 07:13:27 +1000

    add Listener Abstract Class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 //
 // This is a simple NATS streaming client that listens for messages on the "ticket:created" channel.
@@ -39,3 +39,46 @@ stan.on("connect", () => {
 
 process.on("SIGINT", () => stan.close()); // Close NATS connection on SIGINT. This is typically sent when the user presses Ctrl+C in the terminal.
 process.on("SIGTERM", () => stan.close()); // Close NATS connection on SIGTERM. This is typically sent when the process is terminated by a signal from the operating system.
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void; // Abstract method to handle incoming messages
+  private client: Stan;
+  protected ackWait = 5 * 1000; // Acknowledge wait time in milliseconds
+  protected ackTimeout: NodeJS.Timeout | null = null; // Timeout for acknowledgment
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait) // This option is used to set the acknowledgment wait time for the subscription.
+      .setDeliverAllAvailable() // This option is used to deliver all available messages from the stream.
+      .setDurableName(this.queueGroupName); // To create a durable subscription, which allows the subscriber to receive messages even if it is not connected at the time the messages are published.
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg); // Parse the message data
+      this.onMessage(parsedData, msg); // Call the onMessage method with the parsed data and message
+    });
+  }
+  parseMessage(msg: Message) {
+    // This method is used to parse the message data from the NATS message.
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data) // If data is a string, parse it as JSON
+      : JSON.parse(data.toString("utf8")); // Convert Buffer to string and parse as JSON
+  }
+}
