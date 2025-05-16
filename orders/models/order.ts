1b62b973369d2f585741e4db1a6cedc00752b62f
juanLude <jmludevid@hotmail.com>
2025-05-16 14:15:57 +1000

    create order model

---
+++ orders/models/order.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@juanludetickets/common";
+import { TicketDoc } from "./ticket"; // Import the TicketDoc interface
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus), // Use Object.values to get the enum values
+      default: OrderStatus.Created, // Default to Created status
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        // Transform the document before sending it as JSON
+        // Remove the _id and __v fields from the JSON response
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
---
+++ orders/models/ticket.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+interface TicketAttrs {
+  id: string; // This is the id of the ticket in the database
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        // Transform the document before sending it as JSON
+        // Remove the _id and __v fields from the JSON response
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
