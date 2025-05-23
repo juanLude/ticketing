669ea7d99b4499562769aa3e733f325d477de995
juanLude <jmludevid@hotmail.com>
2025-05-23 13:41:50 +1000

    add show test

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../../models/ticket";
+import { Order, OrderStatus } from "../../../models/order";
 console.log("🔥 Test file loaded");
 it("returns an error if the ticket is not found", async () => {
   console.log("ENV:", process.env.NODE_ENV);
@@ -12,6 +14,41 @@ it("returns an error if the ticket is not found", async () => {
     .expect(404);
 });
 
-it("returns an error if the ticket is already reserved", async () => {});
+it("returns an error if the ticket is already reserved", async () => {
+  const ticketId = new mongoose.Types.ObjectId().toHexString(); // Generate a new ObjectId
+  // Create an order with the ticketId
+  const ticket = Ticket.build({
+    id: ticketId,
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    userId: "123",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(400);
+});
 
-it("reserves a ticket", async () => {});
+it("reserves a ticket", async () => {
+  const ticketId = new mongoose.Types.ObjectId().toHexString(); // Generate a new ObjectId
+  const ticket = Ticket.build({
+    id: ticketId,
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(201);
+});
